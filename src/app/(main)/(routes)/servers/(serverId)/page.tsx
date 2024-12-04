806f8606a4d99212e30b5b22c921cc9fb40b70a0
claudion <quangdai1707@gmail.com>
2024-12-04 17:44:23 +0700

    nd

--- src/app/(main)/(routes)/servers/(serverId)/page.tsx
+++
@@ -1,6 +0,0 @@
-const ServerIdPage = () => {
-    return (
-        <div>This is server page</div>
-    );
-}
-export default ServerIdPage;
---
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,10 @@
+import { InitialModal } from "@/components/modals/initial-modal";
+
+const ServerIdPage = () => {
+    // return <InitialModal />
+        return (
+        <div>==============ok===========</div>
+        )
+
+}
+export default ServerIdPage;
